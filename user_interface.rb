1b78ff88c3f60f50c46de6166ef65f1b7b4a5715
Kehinde Aminat <aimenah@outlook.com>
2023-10-17 15:41:30 -0400

    Create user_interface.rb

---
+++ user_interface.rb
@@ -0,0 +1,19 @@
+module UserInterface
+    def self.print_main_menu
+      puts "\n"
+      puts '----------------------------------'
+      puts 'Please choose an option by entering a number'
+      puts '1 - List all books'
+      puts '2 - List all people'
+      puts '3 - Create a person'
+      puts '4 - Create a book'
+      puts '5 - Create a rental'
+      puts '6 - List all rentals for a given person id'
+      puts '7 - Exit'
+      puts "\n"
+    end
+  
+    def self.get_user_input
+      gets.chomp
+    end
+  end
