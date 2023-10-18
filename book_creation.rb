3b12f6ada8cd806ec7a9105569227ba2b8446a22
Ulises Cappa <ulisescappa@gmail.com>
2023-10-18 17:20:54 -0300

    Refactor app.rb. Add book_creaton file to manage the book creation.

---
+++ book_creation.rb
@@ -0,0 +1,12 @@
+require_relative 'book'
+
+class BookCreation
+    def create_book(books)
+        print 'Title: '
+        title = gets.chomp
+        print 'Author: '
+        author = gets.chomp
+        books << Book.new(title, author)
+        puts 'Book created successfully'
+    end
+end
